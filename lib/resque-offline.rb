9768c96bfe6c365a64f030a482ef819737825709
Christopher Durtschi <christopher.durtschi@gmail.com>
2012-07-27 15:53:01 -0600

    Initial commit

---
+++ lib/resque-offline.rb
@@ -0,0 +1,7 @@
+require "resque-offline/version"
+
+module Resque
+  module Offline
+    # Your code goes here...
+  end
+end
---
+++ lib/resque-offline/version.rb
@@ -0,0 +1,5 @@
+module Resque
+  module Offline
+    VERSION = "0.0.1"
+  end
+end
